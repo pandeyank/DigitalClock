a2d43e63a7db750d78b0c2df37f9982d3e2be819
pandeyank <golu.ankit.pandey@gmail.com>
2021-05-28 19:24:36 +0530

    Modifying index.html and clock.js

--- clock.js
+++ clock.js
@@ -1,7 +1,22 @@
-function function()
-{
-    $("#btn1").click(function(){
-        alert("HI");
+setInterval(function (){
+    var currentTime= new Date();
+    var hours= currentTime.getHours();
+    var minutes= currentTime.getMinutes();
+    var seconds= currentTime.getSeconds();
+    var period= "AM";
+
+    if(period>12)
+    {
+        period="PM"
     }
-});   
+    if(hours>12)
+    {
+        hours=hours-12
+    }
+   
+    var clockTime= hours + ":" + minutes + ":" + seconds +" " + period;
+
+    var clock=document.getElementById('clock');
+    clock.innerText= clockTime
+},1000);
 
